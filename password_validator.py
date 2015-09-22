c3d89f3ac6f55c3f12856e8b35f9111a9d1c1ebf
trikitrok <trikitrok@gmail.com>
2015-09-22 15:08:51 +0200

    Only passwords including numbers are strong

--- password_validator.py
+++ password_validator.py
@@ -1,9 +1,12 @@
+import re
+
+
 class PasswordValidator(object):
     def __init__(self, minimum_length):
         self.minimum_length = minimum_length
 
     def is_strong_password(self, password):
-        return self._has_minimum_length(password)
+        return self._has_minimum_length(password) and re.search('[0-9]', password) is not None
 
     def _has_minimum_length(self, password):
         return len(password) >= self.minimum_length
--- password_validator_tests.py
+++ password_validator_tests.py
@@ -9,3 +9,8 @@ def test_a_strong_password():
 def test_that_only_passwords_with_the_minimum_length_are_strong():
     validator = PasswordValidator(8)
     assert validator.is_strong_password("#Ab3ccc") == False
+
+
+def test_that_only_passwords_including_numbers_are_strong():
+    validator = PasswordValidator(8)
+    assert validator.is_strong_password("#Abccccc") == False
