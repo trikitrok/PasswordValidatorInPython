1fd6220d124b542ef1e24bb5a004883ea4825eb1
trikitrok <trikitrok@gmail.com>
2015-09-22 15:01:12 +0200

    Only passwords with the minimum length are strong

--- password_validator.py
+++ password_validator.py
@@ -3,4 +3,4 @@ class PasswordValidator(object):
         self.minimum_length = minimum_length
 
     def is_strong_password(self, password):
-        return True
+        return len(password) >= self.minimum_length
--- password_validator_tests.py
+++ password_validator_tests.py
@@ -4,3 +4,8 @@ from password_validator import PasswordValidator
 def test_a_strong_password():
     validator = PasswordValidator(8)
     assert validator.is_strong_password("#Ab3cccc")
+
+
+def test_a_password_under_the_minimum_length_is_not_strong():
+    validator = PasswordValidator(8)
+    assert validator.is_strong_password("#Ab3ccc") == False
