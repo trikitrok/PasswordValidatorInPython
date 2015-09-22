575a83100d90a5e50286696c5ef3267775a5159b
trikitrok <trikitrok@gmail.com>
2015-09-22 15:56:30 +0200

    Renamed a constant and a static method

--- password_validator.py
+++ password_validator.py
@@ -22,22 +22,22 @@ class PasswordValidator(object):
         return len(password) >= self.minimum_length
 
     def _includes_numbers(self, password):
-        return self._includes(self.NUMBERS, password)
+        return self._includes_any(self.NUMBERS, password)
 
     def _includes_upper_case_letters(self, password):
-        return self._includes(self.UPPER_CASE_LETTERS, password)
+        return self._includes_any(self.UPPER_CASE_LETTERS, password)
 
     def _includes_lower_case_letters(self, password):
-        return self._includes(self.LOWER_CASE_LETTERS, password)
+        return self._includes_any(self.LOWER_CASE_LETTERS, password)
 
     def _includes_special_characters(self, password):
-        return self._includes(self.SPECIAL_CHARACTERS, password)
+        return self._includes_any(self.REQUIRED_SPECIAL_CHARACTERS, password)
 
     @staticmethod
-    def _includes(pattern, password):
+    def _includes_any(pattern, password):
         return re.search(pattern, password) is not None
 
-    SPECIAL_CHARACTERS = '[%#]'
+    REQUIRED_SPECIAL_CHARACTERS = '[%#]'
     UPPER_CASE_LETTERS = '[A-Z]'
     LOWER_CASE_LETTERS = '[a-z]'
     NUMBERS = '[0-9]'
