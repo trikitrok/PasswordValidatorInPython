3d6d259d13996f65adea2ac9203c2c50ceb5666e
trikitrok <trikitrok@gmail.com>
2015-09-22 15:04:21 +0200

    Extracted helper

--- password_validator.py
+++ password_validator.py
@@ -3,4 +3,7 @@ class PasswordValidator(object):
         self.minimum_length = minimum_length
 
     def is_strong_password(self, password):
+        return self._has_minimum_length(password)
+
+    def _has_minimum_length(self, password):
         return len(password) >= self.minimum_length
