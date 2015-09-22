af2f1febc3f279087ba914d41eb270b6151eccac
trikitrok <trikitrok@gmail.com>
2015-09-22 15:04:36 +0200

    Renamed test

--- password_validator_tests.py
+++ password_validator_tests.py
@@ -6,6 +6,6 @@ def test_a_strong_password():
     assert validator.is_strong_password("#Ab3cccc")
 
 
-def test_a_password_under_the_minimum_length_is_not_strong():
+def test_that_only_passwords_with_the_minimum_length_are_strong():
     validator = PasswordValidator(8)
     assert validator.is_strong_password("#Ab3ccc") == False
