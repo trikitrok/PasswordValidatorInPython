2954d2052d67586151b61628c0f41fc2375f836c
trikitrok <trikitrok@gmail.com>
2015-09-23 02:54:55 +0200

    Inlined some helpers

--- password_validator.py
+++ password_validator.py
@@ -6,32 +6,17 @@ class PasswordValidator(object):
         self.minimum_length = minimum_length
 
     def is_strong_password(self, password):
-        includes_special_characters = self._includes_special_characters(password)
-        includes_lower_case_letters = self._includes_lower_case_letters(password)
-        includes_upper_case_letters = self._includes_upper_case_letters(password)
-        includes_numbers = self._includes_numbers(password)
-        has_minimum_length = self._has_minimum_length(password)
+        includes_special_characters = self._includes_any(self.REQUIRED_SPECIAL_CHARACTERS, password)
+        includes_lower_case_letters = self._includes_any(self.LOWER_CASE_LETTERS, password)
+        includes_upper_case_letters = self._includes_any(self.UPPER_CASE_LETTERS, password)
+        includes_numbers = self._includes_any(self.NUMBERS, password)
+        has_minimum_length = len(password) >= self.minimum_length
         return \
             has_minimum_length and \
             includes_numbers and \
             includes_upper_case_letters and \
             includes_lower_case_letters and \
             includes_special_characters
-
-    def _has_minimum_length(self, password):
-        return len(password) >= self.minimum_length
-
-    def _includes_numbers(self, password):
-        return self._includes_any(self.NUMBERS, password)
-
-    def _includes_upper_case_letters(self, password):
-        return self._includes_any(self.UPPER_CASE_LETTERS, password)
-
-    def _includes_lower_case_letters(self, password):
-        return self._includes_any(self.LOWER_CASE_LETTERS, password)
-
-    def _includes_special_characters(self, password):
-        return self._includes_any(self.REQUIRED_SPECIAL_CHARACTERS, password)
 
     @staticmethod
     def _includes_any(pattern, password):
